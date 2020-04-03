d34dc8953aefbca8c59e5b7e481c2e0fcb776883
Matt Kleman <matt.kleman02@gmail.com>
2020-04-03 17:24:44 -0400

    Cleanup and comments

--- wave.c
+++ wave.c
@@ -53,31 +53,37 @@ int main(int argc, char **argv)
 	if ( header.formatChunk.bitsPerSample != 16 )
 		return printError(INVALID_SAMPLE_SIZE);
 
+	//Get the number of samples needed to allocate memory for the channels
 	unsigned int numSamples = numSamplesCalc(&header);
 
 	short* leftChannel = (short*)malloc(numSamples * sizeof(short));
 	short* rightChannel = (short*)malloc(numSamples * sizeof(short));
+
+	//Used to check for error
 	if ( leftChannel == NULL || rightChannel == NULL)
 		return printError(OUT_OF_MEM);
 
+	//Get the characters of the frist sample
 	int a = getchar();
-	int b;
+	int b = getchar();
 	short value;
-	if ( a == EOF )
-		return printError(BAD_FORMAT_CHUNK);
-	else {
-		b = getchar();
-	}
 
-	// Storing the data
 	int leftIndex = 0;
 	int rightIndex = 0;
+
+	//Loop up to 2 times the number of samples because we need to store
+	//the number of samples for each channel
 	for ( int i = 0; i < numSamples*2; ++i ) {
+		//Used to check for error if we hit the EOF before expected
 		if ( b == EOF )
 			return printError(INVALID_FILE_SIZE);
 		else {
+			//Combine the character values into a short
 			value = b << 8;
 			value = value | a;
+
+			//Store in the left channel on even iterations and the right channel
+			//on odd iterations
 			if ( (i % 2) == 0) {
 				leftChannel[leftIndex] = value;
 				++leftIndex;
@@ -87,6 +93,7 @@ int main(int argc, char **argv)
 				++rightIndex;
 			}
 		}
+		//Get the characters for the next sample
 		a = getchar();
 		b = getchar();
 	}
@@ -96,16 +103,20 @@ int main(int argc, char **argv)
 	int currentArg = 1;
 	while (currentArg < argc) {
 		if (strcmp( "-r", argv[currentArg]) == 0) {
-			// reverse sound
+			// Reverse sound
 			reverse(leftChannel, &header);
 			reverse(rightChannel, &header);
 		} else if (strcmp("-s", argv[currentArg]) == 0) {
-			// change speed
+			// Change speed
 			currentArg++;
+			// Convert the string factor into a double
 			double speedFactor = atof(argv[currentArg]);
+			// Used to check for error
 			if ( speedFactor <= 0.0 )
 				return printError(INVALID_SPEED);
-
+			// Change the speed of the right and left channels
+			// changeSpeed returns an int equal to the bytes added for the
+			// change in the channel sizes used to update the header
 			int bytesAdded = changeSpeed(&leftChannel, &header, speedFactor);
 			bytesAdded = changeSpeed(&rightChannel, &header, speedFactor);
 			header.dataChunk.size += bytesAdded;
@@ -117,45 +128,51 @@ int main(int argc, char **argv)
 			leftChannel = rightChannel;
 			rightChannel = temp;
 		} else if (strcmp("-o", argv[currentArg]) == 0) {
-			// fade out
+			// Fade out
 			currentArg++;
 			double fadeTime = atof(argv[currentArg]);
+			//Used to check for error
 			if ( fadeTime <= 0.0 )
 				return printError(INVALID_TIME);
 
 			fadeOut(leftChannel, &header, fadeTime);
 			fadeOut(rightChannel, &header, fadeTime);
 		} else if (strcmp("-i", argv[currentArg]) == 0) {
-			// fade in
+			// Fade in
 			currentArg++;
 			double fadeTime = atof(argv[currentArg]);
+			//Used to check for error
 			if ( fadeTime <= 0.0)
 				return printError(INVALID_TIME);
-
 
 			fadeIn(leftChannel, &header, fadeTime);
 			fadeIn(rightChannel, &header, fadeTime);
 		} else if (strcmp("-v", argv[currentArg]) == 0) {
-			// volume
+			// Change volume
 			currentArg++;
 			double volumeFactor = atof(argv[currentArg]);
+			//Used to check for error
 			if ( volumeFactor <= 0.0 )
 				return printError(INVALID_VOLUME);
 
 			changeVolume(leftChannel, &header, volumeFactor);
 			changeVolume(rightChannel, &header, volumeFactor);
 		} else if (strcmp("-e", argv[currentArg]) == 0) {
-			// echo
+			// Echo
 			currentArg++;
 			double delay = atof(argv[currentArg]);
+			//Used to check for error
 			if ( delay <= 0.0 )
 				return printError(INVALID_ECHO);
 
 			currentArg++;
 			double echoVolumeFactor = atof(argv[currentArg]);
+			//Used to check for error
 			if ( echoVolumeFactor <= 0.0 )
 				return printError(INVALID_ECHO);
 
+			// echo returns an int equal to the bytes added for the
+			// change in the channel sizes used to update the header
 			int bytesAdded = echo(&leftChannel, &header, delay, echoVolumeFactor);
 			echo(&rightChannel, &header, delay, echoVolumeFactor);
 			header.dataChunk.size += bytesAdded;
@@ -168,8 +185,10 @@ int main(int argc, char **argv)
 	/* Write data into stdout */
 	writeHeader(&header);
 
+	//Get the new sample size after the .wav file is changed
 	numSamples = numSamplesCalc(&header);
 
+	//Undo the characters to short converstion that was done when storing data
 	for( int i = 0; i < numSamples; ++i ) {
 		putchar(leftChannel[i] & 0xFF);
 		putchar(leftChannel[i] >> 8);
@@ -177,6 +196,7 @@ int main(int argc, char **argv)
 		putchar(rightChannel[i] >> 8);
 	}
 
+	//Free the allocated memory
 	free(leftChannel);
 	free(rightChannel);
 	return NO_ERROR;
@@ -208,11 +228,8 @@ int readHeader( WaveHeader* header )
 /* returns the number of sampels in one channel */
 unsigned int numSamplesCalc( WaveHeader* header )
 {
-	unsigned int numBytes = header->dataChunk.size;
-	unsigned short numChannels = header->formatChunk.channels;
-	unsigned short bitsPerSample = header->formatChunk.bitsPerSample;
-
-	unsigned int numSamples = numBytes / (numChannels * (bitsPerSample / 8));
+	unsigned int numSamples = header->dataChunk.size / (header->formatChunk.channels *
+		(header->formatChunk.bitsPerSample / 8));
 	return numSamples;
 }
 
